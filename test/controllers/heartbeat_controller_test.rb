e41483d344658c875bf2a9c68cecd33c483010c0
Morgan Kesler <keslerm@dasbiersec.com>
2015-10-15 21:17:04 -0400

    Fixing unit tests for new heartbeat controller

--- test/controllers/heartbeat_controller_test.rb
+++ test/controllers/heartbeat_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class HeartbeatControllerTest < ActionController::TestCase
-  test "should get beat" do
-    get :beat
+  test "should return error for invalid token" do
+    post :beat, { 'name' => 'server1', 'token' => 'test111'}, format: :json
+    assert_response 401
+  end
+
+  test "should update ip and last_heartbeat" do
+    post :beat, { 'name' => 'server1', 'token' => 'test123'}, format: :json
     assert_response :success
   end
 
--- test/controllers/server_controller_test.rb
+++ test/controllers/server_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class ServerControllerTest < ActionController::TestCase
-  test "should return error for invalid token" do
-    post :heartbeat, { 'name' => 'server1', 'token' => 'test111'}, format: :json
-    assert_response 401
-  end
-
-  test "should update ip and last_heartbeat" do
-    post :heartbeat, { 'name' => 'server1', 'token' => 'test123'}, format: :json
-    assert_response :success
-  end
 
 end
