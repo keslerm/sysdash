d210e2abd760de7f8bb7d87e23654856f09b64e5
Morgan Kesler <keslerm@dasbiersec.com>
2015-10-19 08:25:09 -0400

    Fixing unit tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,10 +18,10 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: {  }
+      post :create, user: { email: 'test@test.com', password: 'password1' }
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to assigns(:users)
   end
 
   test "should show user" do
@@ -35,8 +35,8 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    patch :update, id: @user, user: {  }
-    assert_redirected_to user_path(assigns(:user))
+    patch :update, id: @user, user: { id: 1, email: 'user@email.com', password: '12345' }
+    assert_redirected_to assigns(:users)
   end
 
   test "should destroy user" do
