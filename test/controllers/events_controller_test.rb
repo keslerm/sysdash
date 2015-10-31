822ea68d1ef30ced5c792288eef84b4d8092c123
Morgan Kesler <keslerm@dasbiersec.com>
2015-10-31 09:17:20 -0400

    Style cleanup

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
   test "should get create" do
-    post :create, { 'message' => 'Hello world!', 'name' => 'server1', 'token' => 'test123' }
+    post :create, { message: 'Hello world!', name: 'server1', token: 'test123' }
     assert_response :success
   end
 
