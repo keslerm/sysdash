a73e75cc405eca8e8f86cf329c6247b1d4f8c844
Morgan Kesler <keslerm@dasbiersec.com>
2015-11-04 22:27:16 -0500

    Adding test for logout

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+  test 'should destroy session on logout' do
+    get :destroy, nil, {user_id: 1}
+    assert_redirected_to '/login'
+
+    assert_same nil, session[:user_id]
+  end
 end
